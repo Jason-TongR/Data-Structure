74b6eba6b2342593c2750dab46a7d07d6257a6c3
Ziyun shen <2508367326@qq.com>
2026-04-18 21:55:57 +0800

    Nube完成了Exercise6全部

--- Main.java
+++ Main.java
@@ -23,33 +23,78 @@ class Node {
     }
 
     static int slowHeight(Node p) {
-        // TODO: COMPLETE
-        return 0;
+        if (p == null){
+            return 0;
+        }else{
+            return 1 + Math.max(slowHeight(p.left) , slowHeight(p.right));
+        }
+        // Worst-case time complexity is O(n)
     }
 
     static int fastHeight(Node p) {
-        // TODO: COMPLETE
-        return 0;
+        if (p == null){
+            return 0;
+        }else{
+        return p.height;
+        }
+        // Worst-case time complexity is O(1)    
     }
 
     static int balanceFactor(Node p) {
-        // TODO: COMPLETE
-        return 0;
+        if (p == null){
+            return 0;
+        }else{
+            return fastHeight(p.left) - fastHeight(p.right);
+        }
+        // Worst-case time complexity is O(1)  
     }
 
     static boolean checkInvariant(Node p) {
-        // TODO: COMPLETE
-        return false;
+        // Empty tree satisfies the invariant
+        if (p == null){
+            return true;
+        }
+        
+        // The key of the root is right(compare with leftC and rightC)
+        if (p.left != null && p.left.key >= p.key){
+            return false;
+        }
+        if (p.right != null && p.right.key <= p.key){
+            return false;
+        }
+        
+        // The correctness for the height
+        int actualHeight = slowHeight(p);
+        if (actualHeight != fastHeight(p)){
+            return false;
+        }
+        
+        // BalanceFactor must be 1 0 0r -1
+        int num = balanceFactor(p);
+        if (num < -1 || num > -1){
+            return false;
+        }
+        
+        // Checking left and right subtree recursively
+        return checkInvariant(p.left) && checkInvariant(p.right);
     }
 
     static void testInvariant() {
+        // Construct the root
         Node p = new Node(5);
         p.height = 3;
+        
+        // Construct the left-subtree
         p.left = new Node(2);
-        // p.left.height = ...
-        // p.right = ...
-        //
-        // TODO: continue building the tree...
+        p.left.height = 1;
+        
+        // Construct the right-subtree
+        p.right = new Node(7);
+        p.right.height = 2;
+        p.right.left = new Node(6);
+        p.right.left.height = 1;
+        p.right.right = new Node(9);
+        p.right.right.height = 1;
  
         if (checkInvariant(p)) {
             System.out.println("Invariant is fulfilled.");
